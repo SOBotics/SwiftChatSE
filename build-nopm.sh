e44d112df2ec7e9182483f2a4451b2a8fabc0a20
NobodyNada <jkeller234@icloud.com>
2016-11-29 15:33:59 -0800

    stop building on error

--- build-nopm.sh
+++ build-nopm.sh
@@ -1,9 +1,9 @@
 [[ -d Clibwebsockets ]] && rm -rf Clibwebsockets
-git clone git://github.com/NobodyNada/Clibwebsockets
+git clone git://github.com/NobodyNada/Clibwebsockets || exit 1
 
 echo "Generating library..."
-swiftc -IClibwebsockets -L/usr/local/lib -I/usr/local/opt/openssl/include -I/usr/local/include -emit-library -emit-object Sources/SwiftChatSE/*.swift -module-name SwiftChatSE
-ar rcs libSwiftChatSE.a *.o
+swiftc -IClibwebsockets -L/usr/local/lib -I/usr/local/opt/openssl/include -I/usr/local/include -emit-library -emit-object Sources/SwiftChatSE/*.swift -module-name SwiftChatSE || exit 1
+ar rcs libSwiftChatSE.a *.o || exit 1
 rm *.o
 echo "Generating swiftmodule..."
-swiftc -IClibwebsockets -L/usr/local/lib -I/usr/local/opt/openssl/include -I/usr/local/include -emit-module Sources/SwiftChatSE/*.swift -module-name SwiftChatSE
+swiftc -IClibwebsockets -L/usr/local/lib -I/usr/local/opt/openssl/include -I/usr/local/include -emit-module Sources/SwiftChatSE/*.swift -module-name SwiftChatSE || exit 1
