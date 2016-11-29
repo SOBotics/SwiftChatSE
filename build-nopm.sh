329f14c9bb5100dfd82faf6c1e35ce7ff5d8f88f
Jonathan Keller <jkeller234@icloud.com>
2016-11-29 12:46:51 -0800

    update build script

--- build-nopm.sh
+++ build-nopm.sh
@@ -1,3 +1,9 @@
-[[ -d Clibwebsockets ]] && rm -rf Clibwebsockets 
+[[ -d Clibwebsockets ]] && rm -rf Clibwebsockets
 git clone git://github.com/NobodyNada/Clibwebsockets
-swiftc -IClibwebsockets -L/usr/local/lib -I/usr/local/opt/openssl/include -I/usr/local/include -emit-library -emit-module Sources/SwiftChatSE/* -module-name SwiftChatSE
+
+echo "Generating library..."
+swiftc -IClibwebsockets -L/usr/local/lib -I/usr/local/opt/openssl/include -I/usr/local/include -emit-library -emit-object Sources/SwiftChatSE/*.swift -module-name SwiftChatSE
+ar rcs libSwiftChatSE.a *.o
+rm *.o
+echo "Generating swiftmodule..."
+swiftc -IClibwebsockets -L/usr/local/lib -I/usr/local/opt/openssl/include -I/usr/local/include -emit-module Sources/SwiftChatSE/*.swift -module-name SwiftChatSE
