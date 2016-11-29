7fc32f88a8b987f8dc84842e81659eb05ee829af
Jonathan Keller <jkeller234@icloud.com>
2016-11-29 12:14:59 -0800

    upload Clibwebsockets to GitHub as a separate package

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "SwiftChatSE",
     dependencies: [
-		.Package(url: "Clibwebsockets", majorVersion: 1)
+		.Package(url: "git://github.com/NobodyNada/Clibwebsockets", majorVersion: 1)
 	]
 )
--- build-nopm.sh
+++ build-nopm.sh
@@ -1,1 +1,3 @@
-swiftc -I. -L/usr/local/lib -emit-library -emit-module Sources/SwiftChatSE/* -module-name SwiftChatSE
+[[ -d Clibwebsockets ]] && rm -rf Clibwebsockets 
+git clone git://github.com/NobodyNada/Clibwebsockets
+swiftc -IClibwebsockets -L/usr/local/lib -I/usr/local/opt/openssl/include -I/usr/local/include -emit-library -emit-module Sources/SwiftChatSE/* -module-name SwiftChatSE
