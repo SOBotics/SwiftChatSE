24fd5ff0a65bfe254c296939c1722e26559987f6
Jonathan Keller <jkeller234@icloud.com>
2016-12-17 14:58:17 -0800

    cleaned up lots of code

--- Sources/SwiftChatSE/ChatMessage.swift
+++ Sources/SwiftChatSE/ChatMessage.swift
@@ -8,12 +8,23 @@
 
 import Foundation
 
-open class ChatMessage {
-	open let room: ChatRoom
-    open let user: ChatUser
-    open let content: String
-    open let id: Int?
-	open let replyID: Int?
+///A ChatMessage represents a message in Stack Exchange chat.
+public struct ChatMessage {
+	///The room in which this message was posted.
+	public let room: ChatRoom
+	
+	///The user who posted this message.
+    public let user: ChatUser
+	
+	///The content of this message.
+    public let content: String
+	
+	///This message's ID, or `nil` if it is not known.
+    public let id: Int?
+	
+	///The message that this message replied to, or `nil` if this message is not a reply.
+	public let replyID: Int?
+	
 	
 	public init(room: ChatRoom, user: ChatUser, content: String, id: Int?, replyID: Int? = nil) {
 		self.room = room
