fa461a040c164a5dd59d6b79978afa624f68c019
Jonathan Keller <jkeller234@icloud.com>
2017-04-19 10:41:00 -0700

    Revert "temporary debug logging"
    This reverts commit 04c5e3a9c7a2c4cf44f07538a27fd3e9c0cee16c.

--- Sources/SwiftChatSE/Client.swift
+++ Sources/SwiftChatSE/Client.swift
@@ -232,8 +232,6 @@ open class Client: NSObject, URLSessionDataDelegate {
 		didReceive response: URLResponse,
 		completionHandler: @escaping (URLSession.ResponseDisposition) -> Void) {
 		
-		print("Recieved response")
-		
 		guard let task = tasks[dataTask] else {
 			print("\(dataTask) is not in client task list; cancelling")
 			completionHandler(.cancel)
@@ -277,7 +275,6 @@ open class Client: NSObject, URLSessionDataDelegate {
 			return
 		}
 		task.error = error
-		print("Task completed")
 		
 		task.completion(task.data, task.response, task.error)
 		
@@ -307,7 +304,6 @@ open class Client: NSObject, URLSessionDataDelegate {
 	}
 	
 	private func performTask(_ task: URLSessionTask, completion: @escaping (Data?, HTTPURLResponse?, Error?) -> Void) {
-		print("Starting task")
 		usleep(500 * 1000)
 		tasks[task] = HTTPTask(task: task, completion: completion)
 		task.resume()
@@ -345,7 +341,6 @@ open class Client: NSObject, URLSessionDataDelegate {
 			
 			if sema.wait(timeout: DispatchTime.now() + 30) == .timedOut {
 				error = RequestError.timeout
-				print("Task timed out")
 			}
 		}
 		
