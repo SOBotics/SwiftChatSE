336acbf109c5f7c0067912847812afc9d0686bce
Jonathan Keller <jkeller234@icloud.com>
2017-06-01 12:43:42 -0700

    build script update for Raspberry Pi

--- build-nopm.sh
+++ build-nopm.sh
@@ -1,9 +1,10 @@
 [[ -d Clibwebsockets ]] && rm -rf Clibwebsockets
 git clone git://github.com/NobodyNada/Clibwebsockets || exit 1
+git clone git://github.com/stephencelis/CSQLite
 
 echo "Generating library..."
-swiftc -IClibwebsockets -L/usr/local/lib -I/usr/local/opt/openssl/include -I/usr/local/include -emit-library -emit-object Sources/SwiftChatSE/*.swift -module-name SwiftChatSE || exit 1
+swiftc -IClibwebsockets -ICSQLite -L/usr/local/lib -I/usr/local/opt/openssl/include -I/usr/local/include -emit-library -emit-object Sources/SwiftChatSE/*.swift -module-name SwiftChatSE || exit 1
 ar rcs libSwiftChatSE.a *.o || exit 1
 rm *.o
 echo "Generating swiftmodule..."
-swiftc -IClibwebsockets -L/usr/local/lib -I/usr/local/opt/openssl/include -I/usr/local/include -emit-module Sources/SwiftChatSE/*.swift -module-name SwiftChatSE || exit 1
+swiftc -IClibwebsockets -ICSqlite -L/usr/local/lib -I/usr/local/opt/openssl/include -I/usr/local/include -emit-module Sources/SwiftChatSE/*.swift -module-name SwiftChatSE || exit 1
