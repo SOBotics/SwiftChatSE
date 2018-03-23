b102644a7005337b4e1fc99ceab75bc09cb32d1d
NobodyNada <jkeller234@icloud.com>
2018-03-23 11:53:25 -0700

    Disable proxy

--- Sources/SwiftChatSE/Client.swift
+++ Sources/SwiftChatSE/Client.swift
@@ -458,7 +458,7 @@ open class Client: NSObject, URLSessionDataDelegate {
         
         super.init()
         
-        configuration.connectionProxyDictionary = [
+        /*configuration.connectionProxyDictionary = [
             "HTTPEnable" : 1,
             kCFNetworkProxiesHTTPProxy as AnyHashable : "192.168.1.234",
             kCFNetworkProxiesHTTPPort as AnyHashable : 8080,
@@ -466,7 +466,7 @@ open class Client: NSObject, URLSessionDataDelegate {
             "HTTPSEnable" : 1,
             kCFNetworkProxiesHTTPSProxy as AnyHashable : "192.168.1.234",
             kCFNetworkProxiesHTTPSPort as AnyHashable : 8080
-        ]
+        ]*/
     }
     
     ///Errors which can occur while logging in.
